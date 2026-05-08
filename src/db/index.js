f5d41c97f09d0c74ac805845a276fecd5117d910
Gopal Sen <gopal.deshra@gmail.com>
2026-05-08 09:03:19 +0530

    modified

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
-import express from "express";
-
-const app = express();
 
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
   } catch (error) {
-    console.error("Error starting the server:", error);
+    console.error("Error connecting to database:", error);
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import connectToDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({ path: "./.env" });
 
 connectToDB()
   .then(() => {
     console.log("Database connection established successfully.");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
   })
   .catch((error) => {
     console.error("Error connecting to database:", error);
