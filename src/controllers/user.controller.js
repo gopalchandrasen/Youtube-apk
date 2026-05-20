4bd2b052fcd0e39155b598fab527c0cd2ab0017a
seng <gopalchandra.sen@wwt.com>
2026-05-20 08:23:17 +0530

    delete .env

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import { uploadFile } from "../utils/cloudinary.util.js"
 
 const getMissingFields = (fields) => {
   return Object.entries(fields)
